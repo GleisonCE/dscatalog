c49152fa47f51fa2032366f722b47cc20040dd39
Gleison Albuquerque <gleison.ce@gmail.com>
2021-09-29 19:36:47 -0300

    Filter component

---
+++ frontweb/src/components/ProductFilter/index.tsx
@@ -0,0 +1,74 @@
+import { ReactComponent as SearchIcon } from "assets/images/search.svg";
+import { Controller, useForm } from "react-hook-form";
+import { Category } from "types/category";
+import Select from "react-select";
+import { useEffect, useState } from "react";
+import { requestBackend } from "util/requests";
+
+import "./styles.css";
+
+type ProductFilterData = {
+  name: string;
+  category: Category;
+};
+
+const ProductFilter = () => {
+  const [selectCategories, setSelectCategories] = useState<Category[]>([]);
+
+  const {
+    register,
+    handleSubmit,
+    control,
+  } = useForm<ProductFilterData>();
+
+  const onSubmit = (formData: ProductFilterData) => {
+    console.log("enviou..");
+  };
+
+  useEffect(() => {
+    requestBackend({ url: "/categories" }).then((response) => {
+      setSelectCategories(response.data.content);
+    });
+  }, []);
+
+  return (
+    <div className="base-card product-filter_container">
+      <form onSubmit={handleSubmit(onSubmit)} className="product-filter-form">
+        <div className="product-filter-name-container">
+          <input
+            {...register("name")}
+            type="text"
+            className="form-control"
+            placeholder="Nome do produto"
+            name="name"
+          />
+          <button className="product-filter-search-icon">
+            <SearchIcon />
+          </button>
+        </div>
+        <div className="product-filter-bottom-container">
+          <div className="product-filter-category-container">
+            <Controller
+              name="category"
+              control={control}
+              render={({ field }) => (
+                <Select
+                  {...field}
+                  options={selectCategories}
+                  isClearable
+                  placeholder="Categoria"
+                  classNamePrefix="product-filter-select"
+                  getOptionLabel={(category: Category) => category.name}
+                  getOptionValue={(category: Category) => String(category.id)}
+                />
+              )}
+            />
+          </div>
+          <button className="btn btn-outline-secondary btn-product-filter-clear">LIMPAR<span className="btn-product-filter-word"> FILTRO</span></button>
+        </div>
+      </form>
+    </div>
+  );
+};
+
+export default ProductFilter;
